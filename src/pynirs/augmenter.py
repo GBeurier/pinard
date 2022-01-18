5b1d8d4324e20bd59d52fc3f102684b8ab544666
gbeurier <beurier@cirad.fr>
2022-01-18 04:04:48 +0100

    - Added y pipeline - Added augmentation pipeline and augmentation 'Union'

--- src/pynirs/augmenter.py
+++ src/pynirs/augmenter.py
@@ -1,5 +1,8 @@
 import warnings
 import numpy as np
+from sklearn.base import TransformerMixin
+import random ## TODO replace by np.random
+import scipy.interpolate as interpolate
 
 ############################
 ##    HELPERS FUNCTIONS   ##
@@ -57,56 +60,169 @@ def nearest_x(x_test,vx):
 #####################
 
 
-# class Generator(TransformerMixin, BaseEstimator):
+class Augmenter(TransformerMixin):
     
-#     def __init__(self, count = 1, *, copy = True):
-#         self.copy = copy
+    def __init__(self, count = 1):
+        self.count = count
 
+    def augment(self, X, y):
+        new_X = X.copy()
+        new_y = y.copy()
+        return new_X, new_y
 
-#     def fit(self, X, y = None):
-#         self._reset()        
-#         return self.partial_fit(X, y)
+    def transform(self, X, y):
+        self.init_size = len(X)
+        for i in range(len(X)):
+            for k in range(self.count):
+                newX, newy = self.augment(X[i], y[i])
+                if isinstance(newX, np.ndarray):
+                    X = np.vstack([X, newX])
+                    y = np.vstack([y, newy])
+        return X, y
     
-#     def partial_fit(self, X, y = None):
-#         if sparse.issparse(X):
-#             raise TypeError("Baseline does not support sparse input")
-        
-#         first_pass = not hasattr(self, "mean_")
-#         X = self._validate_data(
-#             X, 
-#             reset = first_pass, 
-#             dtype = FLOAT_DTYPES, 
-#             estimator = self
-#         )
-        
-#         self.mean_ = np.mean(X, axis = 0)
-#         return self
-
-#     def transform(self, X):
-#         check_is_fitted(self)
-        
-#         X = self._validate_data(
-#             X, 
-#             reset = False, 
-#             copy = self.copy, 
-#             dtype = FLOAT_DTYPES, 
-#             estimator = self
-#         )
-        
-#         X = X - self.mean_
-#         return X
+    def inverse_transform(self, X, y):
+        return X[0:self.init_size], y[0:self.init_size]
     
-#     def inverse_transform(self, X):
-#         check_is_fitted(self)
-
-#         X = check_array(
-#             X, 
-#             copy = self.copy, 
-#             dtype = FLOAT_DTYPES
-#         )
-
-#         X = X + self.mean_
-#         return X
+    def _more_tags(self):
+        return {'allow_nan': False}
     
-#     def _more_tags(self):
-#         return {'allow_nan': False}
+    
+    
+class SampleAugmentation(Augmenter):
+    def augment(self, X, y):
+        return None, None
+    
+class Rotate_Translate(Augmenter):
+    def augment(self, spectrum, y):
+        """ rotate and translate signal """
+        x_range = np.linspace(0, 1, len(spectrum))
+        p2 = random.uniform(-2,2)
+        p1 = random.uniform(-2,2)
+        xI = random.uniform(0,1)
+        yI = random.uniform(0,np.max(spectrum)/3)
+        distor = v_anglePointFixe(x_range,xI,yI,p1,p2)
+        distor = distor*np.std(spectrum)
+        y_distor = spectrum + distor
+        # y_distor[y_distor < 0] = 0
+        return np.array(y_distor), y
+
+class Monotonous_Spline_Simplification(Augmenter):
+    def augment(self, spectrum, y):
+        """ Select regularly spaced points along x_axis and adjust a spline """
+        nfreq = len(spectrum)
+        x_range_real = np.arange(0, nfreq, 1)
+        nb_points = 60
+
+        ctrl_points = np.unique(np.concatenate(([0],random.sample(range(nfreq),nb_points),[nfreq-1])))
+        ctrl_points.sort()
+        x = x_range_real[ctrl_points]
+        y = spectrum[ctrl_points]
+        t, c, k = interpolate.splrep(x, y, s=0, k=3)
+        spline = interpolate.BSpline(t, c, k, extrapolate=False)
+        
+        return spline(x_range_real), y
+
+
+class Dependent_Spline_Simplification(Augmenter):
+    def augment(self, spectrum, y):
+        """ Select regularly spaced points ON the spectrum and adjust a spline """
+        nfreq = len(spectrum)
+        x0 = np.linspace(0, np.max(spectrum), nfreq)
+        res = spectrum_length(x0,spectrum)
+        nb_segments = 10
+        x_samples = []
+        y_samples = []
+
+        for s in range(1,nb_segments):
+            l = spectrum_length(x0,spectrum)[0] / nb_segments
+            # cumulative_length = np.cumsum(np.repeat(l,nb_segments))
+            n_l = s*l
+            test = res[2]
+            toto = interval_selection(n_l,test)
+            P = segment_pt_coord(
+                x1=x0[toto[1]],
+                y1=spectrum[toto[1]],
+                x2=x0[toto[0]],
+                y2=spectrum[toto[0]],
+                fracL=res[1][toto[1]]%l,
+                L=res[1][toto[1]]) 
+
+            x_samples.append(P[0])
+            y_samples.append(P[1])
+        
+        x = np.array(x_samples)
+        x = np.concatenate(([0],x,[np.max(x0)]))
+        y = np.array(y_samples)
+        y = np.concatenate(([spectrum[0]],y,[spectrum[nfreq-1]]))
+        # print(x)
+        t, c, k = interpolate.splrep(x, y, s=0, k=3)
+        xmin, xmax = x.min(), x.max()
+        xx = np.linspace(xmin, xmax, nfreq)
+        spline = interpolate.BSpline(t, c, k, extrapolate=False)
+
+        return spline(xx), y
+
+class Random_Spline_Addition(Augmenter):
+    def augment(self, spectrum, y):
+        """ Add spline noise on y """
+        nfreq = len(spectrum)
+        x_range_real = np.arange(0, nfreq, 1)
+        interval_width = np.max(spectrum)/80
+        half_interval_width = interval_width/2 
+        baseline = random.uniform(-half_interval_width, half_interval_width)
+        interval_min = -half_interval_width + baseline
+        interval_max = half_interval_width + baseline
+
+        nb_spline_points = 40
+        x_points = np.linspace(0, nfreq,nb_spline_points)
+        y_points = [random.uniform(interval_min,interval_max) for i in range(nb_spline_points)]
+
+        x = np.asarray(x_points)
+        x.sort()
+        y = np.asarray(y_points)
+        t, c, k = interpolate.splrep(x, y, s=0, k=3)
+
+        spline = interpolate.BSpline(t, c, k, extrapolate=False)
+        distor = spline(x_range_real)
+        y_distor = spectrum + distor
+        # y_distor[y_distor < 0] = 0
+        return np.array(y_distor), y
+
+class Random_Y_Shift(Augmenter):
+    def augment(self, spectrum, y):
+        """ Additive delta on y """
+        spec_range = np.max(spectrum) - np.min(spectrum)
+        y_distor = spectrum + random.uniform(-spec_range,spec_range) / 20
+        return y_distor, y
+
+class Random_Multiplicative_Shift(Augmenter):
+    def augment(self, spectrum, y):
+        """ Multiplicative delta on y """
+        y_distor = spectrum * random.uniform (-0.05, 0.05)
+        return y_distor, y
+
+
+class Random_X_Spline_Deformation(Augmenter):
+    def augment(self, spectrum, y):
+        """ Random modification of x based on subsampled spline """
+        x = np.arange(0, len(spectrum), 1)
+        t, c, k = interpolate.splrep(x, spectrum, s=0, k=3)
+
+        delta_x_size = int(np.around(len(t)/20))
+        delta_x = np.linspace(np.min(x), np.max(x), delta_x_size)
+        delta_y = np.random.uniform(-10,10,delta_x_size)
+        delta = np.interp(t, delta_x, delta_y)
+        t = t + delta
+        spline = interpolate.BSpline(t, c, k, extrapolate=True)
+        return spline(x), y
+
+class Random_X_Spline_Shift(Augmenter):
+    def augment(self, spectrum, y):
+        """ Add spline based x shift """
+        x = np.arange(0, len(spectrum), 1)
+        delta = random.uniform(-10,10)
+        x = x + delta
+        t, c, k = interpolate.splrep(x, spectrum, s=0, k=3)
+        spline = interpolate.BSpline(t, c, k, extrapolate=False)
+        return spline(x), y
+
--- src/pynirs/pipeline_tools.py
+++ src/pynirs/pipeline_tools.py
@@ -3,7 +3,7 @@ from joblib import Parallel, delayed
 from scipy import sparse
 import numpy as np
 
-class FeatureUnionOnAxis(FeatureUnion):
+class FeatureUnionNewAxis(FeatureUnion):
     axis = 0
     
     def __init__(self, transformer_list, n_jobs=None, transformer_weights=None, axis=0):
@@ -13,8 +13,7 @@ class FeatureUnionOnAxis(FeatureUnion):
     def fit_transform(self, X, y=None, **fit_params):
         self._validate_transformers()
         result = Parallel(n_jobs=self.n_jobs)(
-            delayed(_fit_transform_one)(trans, X, y, weight,
-                                        **fit_params)
+            delayed(_fit_transform_one)(trans, X, y, weight,**fit_params)
             for name, trans, weight in self._iter())
 
         if not result:
@@ -22,16 +21,17 @@ class FeatureUnionOnAxis(FeatureUnion):
             return np.zeros((X.shape[0], 0))
         Xs, transformers = zip(*result)
         self._update_transformer_list(transformers)
-        Xs = np.concatenate(Xs, axis=self.axis)
+        Xs = np.swapaxes(Xs, 0, 1)
+        Xs = np.swapaxes(Xs, 1, 2)
         return Xs    
     
     def transform(self, X):
-        print('transform')
         Xs = Parallel(n_jobs=self.n_jobs)(
             delayed(_transform_one)(trans, X, None, weight)
             for name, trans, weight in self._iter())
         if not Xs:
             # All transformers are None
             return np.zeros((X.shape[0], 0))
-        Xs = np.concatenate(Xs, axis=self.axis)
+        Xs = np.swapaxes(Xs, 0, 1)
+        Xs = np.swapaxes(Xs, 1, 2)
         return Xs
