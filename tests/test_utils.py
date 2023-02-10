2a6dc12f052c99db6b0f0b06c576ad6ee86f720f
Gregory Beurier <beurier@cirad.fr>
2023-02-10 14:33:35 +0100

    Fixed remove_na in

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,7 @@ def test_single_file_load(simple_data):
 
 
 def test_single_file_load_na(simple_data_na):
-    x, y = utils.load_csv(simple_data_na, y_cols=[0, 1], x_hdr=0, x_index_col=[0, 1], remove_na=True)
+    x, y = utils.load_csv(simple_data_na, y_cols=[0, 1], x_hdr=0, x_index_col=[0, 1], autoremove_na=True)
     assert x.shape == (4, 3)
     assert x[0, 1] == 2
 
@@ -32,14 +32,7 @@ def test_double_file_load(simple_data, simple_data_na):
 def test_double_file_load_na(simple_data, simple_data_na):
     try:
         x, y = utils.load_csv(
-            simple_data,
-            simple_data_na,
-            y_cols=[0, 1],
-            x_hdr=0,
-            y_hdr=0,
-            x_index_col=[0, 1, 2, 3],
-            y_index_col=[0, 1],
-            remove_na=True
+            simple_data, simple_data_na, y_cols=[0, 1], x_hdr=0, y_hdr=0, x_index_col=[0, 1, 2, 3], y_index_col=[0, 1], autoremove_na=True
         )
     except utils.WrongFormatError as error:
         assert error.x.shape == (6, 3)
