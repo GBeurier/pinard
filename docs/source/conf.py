b716e69e2dbd23bd19e802f6dac91097145ec40f
Gregory Beurier <beurier@cirad.fr>
2023-11-16 17:01:33 +0100

    rtd config update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,7 @@ release = '1.0'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
-    'sphinx_rtd_theme',
-    'sphinx_autodoc_typehints'
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
